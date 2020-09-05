281d69592fe162b851ec5b45b82bdfeae678cbe2
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-05 05:47:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+}
+
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+
+function appendKitten(name) {
+  kittens.concat(name)
+}
